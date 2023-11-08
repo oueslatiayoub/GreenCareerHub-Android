9ed44b452ee90a4d3f53902bb8cafca4a23f4262
ayoub oueslati <80178145+oueslatiayoub@users.noreply.github.com>
2023-11-08 16:03:08 +0100

    Initial commit

---
+++ app/src/main/java/tn/esprit/gestionuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package tn.esprit.gestionuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
